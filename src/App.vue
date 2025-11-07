f1c7b1feb0494e56c91be3e47b6545e9c79fd05d
202413059-cloud <202413059@jbnu.ac.kr>
2025-11-08 01:29:24 +0900

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,20 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>✅ Vue 3 연결 성공!</h1>
+  <p>이 문장이 화면에 보이면 App.vue 수정이 반영된 거예요.</p>
 </template>
 
+<script setup lang="ts">
+// 아무 코드 없어도 됩니다.
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
+h1 {
+  color: #42b883;
+  text-align: center;
+  margin-top: 100px;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+p {
+  text-align: center;
+  font-size: 18px;
 }
 </style>
